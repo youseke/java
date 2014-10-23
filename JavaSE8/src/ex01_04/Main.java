a7ce347bf12b9d95fc42289959328de93031d3b1
Tohtetsu Choh <tohtetsu.choh@nts.ricoh.co.jp>
2014-10-24 07:27:02 +0900

    Fix

--- JavaSE8/src/ex01_04/Main.java
+++ JavaSE8/src/ex01_04/Main.java
@@ -6,17 +6,18 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
+        File f = new File("/");
+        System.out.println(Arrays.toString(sortsFiles(f.listFiles())));
     }
 
     public static File[] sortsFiles(File[] files) {
         Arrays.sort(files, (File o1, File o2) -> {
             if (o1.isDirectory()) {
                 if (!o2.isDirectory())
-                    return 1;
+                    return -1;
             } else {
                 if (o2.isDirectory())
-                    return -1;
+                    return 1;
             }
             return Integer
                     .compare(o1.getPath().length(), o2.getPath().length());
--- JavaSE8/src/ex01_09/TestCollection2.java
+++ JavaSE8/src/ex01_09/TestCollection2.java
@@ -1,13 +1,15 @@
 package ex01_09;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Tohtetsu Choh
  */
 public class TestCollection2 {
-
+    
     public static void main(String[] args) {
-        Collection2<Integer> numbers = null;
+        Collection2<Integer> numbers = Arrays.asList(10, 25, 5, 100, 0, 20, 8);
         numbers.forEachIf(p -> System.out.println(p), p -> p > 10);
     }
 }
