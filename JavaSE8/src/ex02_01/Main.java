81931f8152d51f186db7d9cfe7bdc72337ed006b
Tohtetsu Choh <tohtetsu.choh@nts.ricoh.co.jp>
2014-10-24 11:31:07 +0900

    Fix ex02_01

--- JavaSE8/src/ex02_01/Main.java
+++ JavaSE8/src/ex02_01/Main.java
@@ -12,22 +12,22 @@ import java.util.List;
  * @author Tohtetsu Choh
  */
 public class Main {
-
-    static int count = 0;
-
-    public static void main(String[] args) throws IOException {
+    
+    public static void main(String[] args) throws IOException, InterruptedException {
+        long[] count = {0, 0};
         System.out.println("Starting...");
         String contents = new String(Files.readAllBytes(Paths.get("src\\alice.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
         for (String w : words) {
             new Thread(() -> {
                 if (w.length() > 1) {
-                    Main.count++;
+                    count[0]++;
                 }
-            });
+            }).start();
         }
-
-        System.out.println(count);
+        Thread.sleep(2000);
+        count[1] = words.stream().count();
+        System.out.println(Arrays.toString(count));
         System.out.println("End");
     }
 }
