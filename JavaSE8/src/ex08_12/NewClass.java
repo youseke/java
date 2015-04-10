3cacf472383a39c337a45361ce303a05e1d06ea3
Tohtetsu <Tohtetsu@10.60.163.111>
2015-04-10 14:30:09 +0900

    Bug fix.

--- JavaSE8/src/ex08_12/NewClass.java
+++ JavaSE8/src/ex08_12/NewClass.java
@@ -6,7 +6,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.reflect.Method;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 /**
@@ -43,5 +45,18 @@ public class NewClass {
     public @interface TestCases {
 
         TestCase[] value();
+    }
+    
+    static class Math {
+
+        @TestCase(params = 4, expected = 24)
+        @TestCase(params = 0, expected = 1)
+        public static int factorial(int n) {
+            int fact = 1;
+            for (int i = 1; i <= n; i++) {
+                fact *= i;
+            }
+            return fact;
+        }
     }
 }
